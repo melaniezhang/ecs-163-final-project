ee0e8106576ffa82a8d6941b90dd0ba40caffc64
Sarah Quick <snquick@ucdavis.edu>
2018-03-18 14:57:54 -0700

    Add Sunburst code

---
+++ Sunburst/sunburst.js
@@ -0,0 +1,264 @@
+// sunburst.js
+// Author: Sarah Quick
+// ECS163 Final Project
+
+// Resources:
+// Max's Interactivity Example
+// Sunburst Tutorial: https://gist.github.com/denjn5/e1cdbbe586ac31747b4a304f8f86efa5
+// Sunburst Example: https://bl.ocks.org/mbostock/4063423
+// Color Scheme Colors: https://stackoverflow.com/questions/17569205/d3-scale-category20b-always-returning-first-color
+// D3 API: https://github.com/d3/d3/blob/master/API.md
+// d3 Hierarchy API: https://github.com/d3/d3-hierarchy/blob/master/README.md#treemap
+
+// JSON data
+var nodeData = {
+    "name": "TOPICS", "children": [{
+        "name": "Topic A",
+        "children": [{"name": "Sub A1", "size": 10}, {"name": "Sub A2", "size": 40}]
+    }, {
+        "name": "Topic B",
+        "children": [{"name": "Sub B1", "size": 30}, {"name": "Sub B2", "size": 30}, {
+            "name": "Sub B3", "size": 30}]
+    }, {
+        "name": "Topic C",
+        "children": [{"name": "Sub A1", "size": 40}, {"name": "Sub A2", "size": 50}]
+    }]
+};
+
+function computeTextRotation(d) {
+    var angle = (d.x0 + d.x1) / Math.PI * 90;
+    // Avoid upside-down labels
+    return (angle < 120 || angle > 270) ? angle : angle + 180;  // labels as rims
+    //return (angle < 180) ? angle - 90 : angle + 90;  // labels as spokes
+}
+
+
+function Sunburst(category) {
+
+// Variables
+    var width = 500;
+    var height = 500;
+    var radius = Math.min(width, height) / 2;
+    var color = d3.scaleOrdinal(d3.schemeCategory20b);
+
+// Create svg
+    var svg = d3.select('body').append('svg')
+        .attr("class", "Sunburst")
+        .attr('width', width * 2)
+        .attr('height', height);
+/*
+    // Create svg
+    var svg2 = d3.select('body').append('svg')
+        .attr("class", "Sunburst")
+        .attr('width', width)
+        .attr('height', height);
+*/
+    this.update = function (category) {
+
+        // Remove existing sunBurst
+        svg.selectAll('*').remove();
+
+        // append group to svg
+        var g = svg.append('g')
+            .attr('transform', 'translate(' + width / 2 + ',' + height / 2 + ')');
+
+        // Initializes partition layout and sets size
+        var partition = d3.partition()
+            .size([2 * Math.PI, radius]);
+
+        // Format data into hierarchy
+        var root = d3.hierarchy(category)
+        // Sums up all sizes to get the size of the sunburst diagram
+            .sum(function (d) {
+                return d.size
+            });
+
+        // Lays out hierarchy of root
+        partition(root);
+
+        // Calculates size of each arc from data
+        var arc = d3.arc()
+            .startAngle(function (d) {
+                return d.x0
+            })
+            .endAngle(function (d) {
+                return d.x1
+            })
+            .innerRadius(function (d) {
+                return d.y0
+            })
+            .outerRadius(function (d) {
+                return d.y1
+            });
+
+        // Uses children of root in data to draw correct path,
+        g.selectAll('path')
+            .data(root.descendants()) // Get's data from children of root
+            .enter().append('g')
+            .append('path') // Create path
+            .attr("display", function (d) {
+                return d.depth ? null : "none";
+            }) // Get depth of  node, if no depth, don't display
+            .attr("d", arc) // fills in "d" with arc info
+            .style("fill", function (d) {
+                return color((d.children ? d : d.parent).data.name);
+            }) // Chooses arc color based on parent color
+
+        .on('mouseover', function(datum, index, nodes) {
+            // select our tooltip
+            var tooltip = d3.select('#hoverText');
+
+            // make sure our tooltip is going to be displayed
+            tooltip.style('display', 'block');
+
+            // set the initial position of the tooltip
+            tooltip.style('left', d3.event.pageX);
+            tooltip.style('top', d3.event.pageY);
+
+            tooltip.html(function(d) { return d.parent; });
+        })
+        .on('mouseleave', function(datum, index, nodes) {
+            var tooltip = d3.select('#hoverText');
+            tooltip.style('display', 'none');
+        })
+
+        //Make Legend
+        var yMult = 25;
+        var yPos = ((height)/3);
+        var xBoxPos = 600;
+        var xTextPos = xBoxPos + 40;
+        var xTextOffset = 15;
+        /*
+        // Main Box
+        var legend = svg2.append("rect")
+            .attr("width", width)
+            .attr("height", height)
+            .attr("x", 2 * width)
+            .attr("y", (height/ 2))
+            .style("stroke", "black")
+            .attr("fill", "white");
+        */
+        svg.append("text")
+            .attr("x", 600)
+            .attr("y", (height)/3 - 33)
+            .attr("font-size", "small")
+            .attr("font-weight", "bold")
+            .text("Percentage");
+
+        svg.append("text")
+            .attr("x", 600)
+            .attr("y", (height)/3 - 20)
+            .attr("font-size", "small")
+            .attr("font-weight", "bold")
+            .text("of Goal");
+
+        svg.append("text")
+            .attr("x", 600)
+            .attr("y", (height)/3 - 7)
+            .attr("font-size", "small")
+            .attr("font-weight", "bold")
+            .text("Reached");
+
+        // First Box
+        svg.append("rect")
+            .attr("width", 30)
+            .attr("height", 20)
+            .attr("x", xBoxPos)
+            .attr("y", yPos + (yMult * 0))
+            .attr("fill", "#5254a3");
+
+        svg.append("text")
+            .attr("x", xTextPos)
+            .attr("y", yPos + (yMult * 0) + xTextOffset)
+            .attr("font-size", "small")
+            .text("0-25");
+
+        // Second Box
+        svg.append("rect")
+            .attr("width", 30)
+            .attr("height", 20)
+            .attr("x", xBoxPos)
+            .attr("y", yPos + (yMult * 1))
+            .attr("fill", "#6b6ecf");
+
+        svg.append("text")
+            .attr("x", xTextPos)
+            .attr("y", yPos + (yMult * 1) + xTextOffset)
+            .attr("font-size", "small")
+            .text("26-50");
+
+        // Third Box
+        svg.append("rect")
+            .attr("width", 30)
+            .attr("height", 20)
+            .attr("x", xBoxPos)
+            .attr("y", yPos + (yMult * 2))
+            .attr("fill", "#9c9ede");
+
+        svg.append("text")
+            .attr("x", xTextPos)
+            .attr("y", yPos + (yMult * 2) + xTextOffset)
+            .attr("font-size", "small")
+            .text("51-75");
+
+        // Fourth Box
+        svg.append("rect")
+            .attr("width", 30)
+            .attr("height", 20)
+            .attr("x", xBoxPos)
+            .attr("y", yPos + (yMult * 3))
+            .attr("fill", "#637939");
+
+        svg.append("text")
+            .attr("x", xTextPos)
+            .attr("y", yPos + (yMult * 3) + xTextOffset)
+            .attr("font-size", "small")
+            .text("76-100");
+
+        // Fifth Box
+        svg.append("rect")
+            .attr("width", 30)
+            .attr("height", 20)
+            .attr("x", xBoxPos)
+            .attr("y", yPos + (yMult * 4))
+            .attr("fill", "#b5cf6b");
+
+        svg.append("text")
+            .attr("x", xTextPos)
+            .attr("y", yPos + (yMult * 4) + xTextOffset)
+            .attr("font-size", "small")
+            .text("101-200");
+
+        // Sixth Box
+        svg.append("rect")
+            .attr("width", 30)
+            .attr("height", 20)
+            .attr("x", xBoxPos)
+            .attr("y", yPos + (yMult * 5))
+            .attr("fill", "#8ca252");
+
+        svg.append("text")
+            .attr("x", xTextPos)
+            .attr("y", yPos + (yMult * 5) + xTextOffset)
+            .attr("font-size", "small")
+            .text("200+");
+
+    }
+
+    this.update(category);
+
+
+}
+
+//alert("Displaying Games");
+
+var sunburst = new Sunburst(games);
+
+//alert("Displaying Crafts");
+
+//sunburst.update(crafts);
+
+
+
+
+
---
+++ Sunburst/sunburstData.js
@@ -0,0 +1,641 @@
+// Crafts:[7818, [5101, 0], [437, 0], [144, 0], [84, 63], [1346, 1346], [706, 706]]
+var crafts = {
+    "name": "Crafts",
+    "children": [
+        {
+            "name": "0-25",
+            "children": [
+                {"name": "successes", "size": 0},
+                {"name": "failures", "size": 5101}
+            ]
+        },
+        {
+            "name": "26-50",
+            "children": [
+                {"name": "successes", "size": 0},
+                {"name": "failures", "size": 437}
+            ]
+        },
+        {
+            "name": "51-75",
+            "children": [
+                {"name": "successes", "size": 0},
+                {"name": "failures", "size": 144}
+            ]
+        },
+        {
+            "name": "76-100",
+            "children": [
+                {"name": "successes", "size": 63},
+                {"name": "failures", "size": 21}
+            ]
+        },
+        {
+            "name": "101-200",
+            "children": [
+                {"name": "successes", "size": 1346},
+                {"name": "failures", "size": 0}
+            ]
+        },
+        {
+            "name": "200+",
+            "children": [
+                {"name": "successes", "size": 706},
+                {"name": "failures", "size": 0}
+            ]
+        }
+    ]
+};
+
+// Fashion:[19775, [12536, 0], [1224, 0], [350, 0], [203, 131], [4191, 4191], [1271, 1271]]
+var fashion = {
+    "name": "Fashion",
+    "children": [
+        {
+            "name": "0-25",
+            "children": [
+                {"name": "successes", "size": 0},
+                {"name": "failures", "size": 12536}
+            ]
+        },
+        {
+            "name": "26-50",
+            "children": [
+                {"name": "successes", "size": 0},
+                {"name": "failures", "size": 1224}
+            ]
+        },
+        {
+            "name": "51-75",
+            "children": [
+                {"name": "successes", "size": 0},
+                {"name": "failures", "size": 350}
+            ]
+        },
+        {
+            "name": "76-100",
+            "children": [
+                {"name": "successes", "size": 131},
+                {"name": "failures", "size": 72}
+            ]
+        },
+        {
+            "name": "101-200",
+            "children": [
+                {"name": "successes", "size": 4191},
+                {"name": "failures", "size": 0}
+            ]
+        },
+        {
+            "name": "200+",
+            "children": [
+                {"name": "successes", "size": 1271},
+                {"name": "failures", "size": 0}
+            ]
+        }
+    ]
+};
+
+// Journalism:[4149, [2949, 0], [145, 0], [34, 0], [62, 53], [844, 844], [115, 115]]
+var journalism = {
+    "name": "Journalism",
+    "children": [
+        {
+            "name": "0-25",
+            "children": [
+                {"name": "successes", "size": 0},
+                {"name": "failures", "size": 2949}
+            ]
+        },
+        {
+            "name": "26-50",
+            "children": [
+                {"name": "successes", "size": 0},
+                {"name": "failures", "size": 145}
+            ]
+        },
+        {
+            "name": "51-75",
+            "children": [
+                {"name": "successes", "size": 0},
+                {"name": "failures", "size": 34}
+            ]
+        },
+        {
+            "name": "76-100",
+            "children": [
+                {"name": "successes", "size": 53},
+                {"name": "failures", "size": 9}
+            ]
+        },
+        {
+            "name": "101-200",
+            "children": [
+                {"name": "successes", "size": 844},
+                {"name": "failures", "size": 0}
+            ]
+        },
+        {
+            "name": "200+",
+            "children": [
+                {"name": "successes", "size": 115},
+                {"name": "failures", "size": 0}
+            ]
+        }
+    ]
+};
+
+// Design:[25364, [11923, 0], [1965, 0], [717, 0], [312, 104], [6053, 6052], [4394, 4394]]
+var design = {
+    "name": "Design",
+    "children": [
+        {
+            "name": "0-25",
+            "children": [
+                {"name": "successes", "size": 0},
+                {"name": "failures", "size": 11923}
+            ]
+        },
+        {
+            "name": "26-50",
+            "children": [
+                {"name": "successes", "size": 0},
+                {"name": "failures", "size": 1965}
+            ]
+        },
+        {
+            "name": "51-75",
+            "children": [
+                {"name": "successes", "size": 0},
+                {"name": "failures", "size": 717}
+            ]
+        },
+        {
+            "name": "76-100",
+            "children": [
+                {"name": "successes", "size": 104},
+                {"name": "failures", "size": 208}
+            ]
+        },
+        {
+            "name": "101-200",
+            "children": [
+                {"name": "successes", "size": 6052},
+                {"name": "failures", "size": 1}
+            ]
+        },
+        {
+            "name": "200+",
+            "children": [
+                {"name": "successes", "size": 4394},
+                {"name": "failures", "size": 0}
+            ]
+        }
+    ]
+};
+
+// Technology:[27050, [18678, 0], [1308, 0], [482, 0], [226, 78], [3698, 3698], [2658, 2658]]
+var technology = {
+    "name": "Technology",
+    "children": [
+        {
+            "name": "0-25",
+            "children": [
+                {"name": "successes", "size": 0},
+                {"name": "failures", "size": 18678}
+            ]
+        },
+        {
+            "name": "26-50",
+            "children": [
+                {"name": "successes", "size": 0},
+                {"name": "failures", "size": 1308}
+            ]
+        },
+        {
+            "name": "51-75",
+            "children": [
+                {"name": "successes", "size": 0},
+                {"name": "failures", "size": 482}
+            ]
+        },
+        {
+            "name": "76-100",
+            "children": [
+                {"name": "successes", "size": 78},
+                {"name": "failures", "size": 148}
+            ]
+        },
+        {
+            "name": "101-200",
+            "children": [
+                {"name": "successes", "size": 3698},
+                {"name": "failures", "size": 0}
+            ]
+        },
+        {
+            "name": "200+",
+            "children": [
+                {"name": "successes", "size": 2658},
+                {"name": "failures", "size": 0}
+            ]
+        }
+    ]
+};
+
+// Music:[45949, [19086, 0], [2073, 0], [522, 0], [929, 860], [21736, 21734], [1603, 1603]]
+var music = {
+        "name": "Music",
+        "children": [
+            {
+                "name": "0-25",
+                "children": [
+                    {"name": "successes", "size": 0},
+                    {"name": "failures", "size": 19086}
+                ]
+            },
+            {
+                "name": "26-50",
+                "children": [
+                    {"name": "successes", "size": 0},
+                    {"name": "failures", "size": 2073}
+                ]
+            },
+            {
+                "name": "51-75",
+                "children": [
+                    {"name": "successes", "size": 0},
+                    {"name": "failures", "size": 522}
+                ]
+            },
+            {
+                "name": "76-100",
+                "children": [
+                    {"name": "successes", "size": 860},
+                    {"name": "failures", "size": 69}
+                ]
+            },
+            {
+                "name": "101-200",
+                "children": [
+                    {"name": "successes", "size": 21734},
+                    {"name": "failures", "size": 2}
+                ]
+            },
+            {
+                "name": "200+",
+                "children": [
+                    {"name": "successes", "size": 1603},
+                    {"name": "failures", "size": 0}
+                ]
+            }
+        ]
+    };
+
+// Art:[25641, [12171, 0], [1435, 0], [424, 0], [551, 450], [9088, 9088], [1972, 1972]]
+var art = {
+    "name": "Art",
+    "children": [
+        {
+            "name": "0-25",
+            "children": [
+                {"name": "successes", "size": 0},
+                {"name": "failures", "size": 12171}
+            ]
+        },
+        {
+            "name": "26-50",
+            "children": [
+                {"name": "successes", "size": 0},
+                {"name": "failures", "size": 1435}
+            ]
+        },
+        {
+            "name": "51-75",
+            "children": [
+                {"name": "successes", "size": 0},
+                {"name": "failures", "size": 424}
+            ]
+        },
+        {
+            "name": "76-100",
+            "children": [
+                {"name": "successes", "size": 450},
+                {"name": "failures", "size": 101}
+            ]
+        },
+        {
+            "name": "101-200",
+            "children": [
+                {"name": "successes", "size": 9088},
+                {"name": "failures", "size": 0}
+            ]
+        },
+        {
+            "name": "200+",
+            "children": [
+                {"name": "successes", "size": 1972},
+                {"name": "failures", "size": 0}
+            ]
+        }
+    ]
+};
+
+// Photography:[9689, [5633, 0], [551, 0], [171, 0], [141, 112], [2699, 2699], [494, 494]]
+var photography = {
+    "name": "Photography",
+    "children": [
+        {
+            "name": "0-25",
+            "children": [
+                {"name": "successes", "size": 0},
+                {"name": "failures", "size": 5633}
+            ]
+        },
+        {
+            "name": "26-50",
+            "children": [
+                {"name": "successes", "size": 0},
+                {"name": "failures", "size": 551}
+            ]
+        },
+        {
+            "name": "51-75",
+            "children": [
+                {"name": "successes", "size": 0},
+                {"name": "failures", "size": 171}
+            ]
+        },
+        {
+            "name": "76-100",
+            "children": [
+                {"name": "successes", "size": 112},
+                {"name": "failures", "size": 141}
+            ]
+        },
+        {
+            "name": "101-200",
+            "children": [
+                {"name": "successes", "size": 2699},
+                {"name": "failures", "size": 0}
+            ]
+        },
+        {
+            "name": "200+",
+            "children": [
+                {"name": "successes", "size": 494},
+                {"name": "failures", "size": 0}
+            ]
+        }
+    ]
+};
+
+// Film & Video:[56527, [29265, 0], [2785, 0], [723, 0], [1140, 1009], [21242, 21242], [1372, 1372]]
+var film_and_video = {
+    "name": "Film & Video",
+    "children": [
+        {
+            "name": "0-25",
+            "children": [
+                {"name": "successes", "size": 0},
+                {"name": "failures", "size": 29265}
+            ]
+        },
+        {
+            "name": "26-50",
+            "children": [
+                {"name": "successes", "size": 0},
+                {"name": "failures", "size": 2785}
+            ]
+        },
+        {
+            "name": "51-75",
+            "children": [
+                {"name": "successes", "size": 0},
+                {"name": "failures", "size": 723}
+            ]
+        },
+        {
+            "name": "76-100",
+            "children": [
+                {"name": "successes", "size": 1009},
+                {"name": "failures", "size": 131}
+            ]
+        },
+        {
+            "name": "101-200",
+            "children": [
+                {"name": "successes", "size": 21242},
+                {"name": "failures", "size": 0}
+            ]
+        },
+        {
+            "name": "200+",
+            "children": [
+                {"name": "successes", "size": 1372},
+                {"name": "failures", "size": 1372}
+            ]
+        }
+    ]
+}
+
+// Food:[22054, [14405, 0], [1177, 0], [340, 0], [207, 160], [5155, 5155], [770, 770]]
+var food = {
+    "name": "Food",
+    "children": [
+        {
+            "name": "0-25",
+            "children": [
+                {"name": "successes", "size": 0},
+                {"name": "failures", "size": 14405}
+            ]
+        },
+        {
+            "name": "26-50",
+            "children": [
+                {"name": "successes", "size": 0},
+                {"name": "failures", "size": 1177}
+            ]
+        },
+        {
+            "name": "51-75",
+            "children": [
+                {"name": "successes", "size": 0},
+                {"name": "failures", "size": 340}
+            ]
+        },
+        {
+            "name": "76-100",
+            "children": [
+                {"name": "successes", "size": 160},
+                {"name": "failures", "size": 47}
+            ]
+        },
+        {
+            "name": "101-200",
+            "children": [
+                {"name": "successes", "size": 5155},
+                {"name": "failures", "size": 0}
+            ]
+        },
+        {
+            "name": "200+",
+            "children": [
+                {"name": "successes", "size": 770},
+                {"name": "failures", "size": 0}
+            ]
+        }
+    ]
+};
+
+// Dance:[3573, [1052, 0], [140, 0], [38, 0], [125, 120], [2154, 2154], [64, 64]]
+var dance = {
+    "name": "Dance",
+    "children": [
+        {
+            "name": "0-25",
+            "children": [
+                {"name": "successes", "size": 0},
+                {"name": "failures", "size": 1052}
+            ]
+        },
+        {
+            "name": "26-50",
+            "children": [
+                {"name": "successes", "size": 0},
+                {"name": "failures", "size": 140}
+            ]
+        },
+        {
+            "name": "51-75",
+            "children": [
+                {"name": "successes", "size": 0},
+                {"name": "failures", "size": 38}
+            ]
+        },
+        {
+            "name": "76-100",
+            "children": [
+                {"name": "successes", "size": 120},
+                {"name": "failures", "size": 5}
+            ]
+        },
+        {
+            "name": "101-200",
+            "children": [
+                {"name": "successes", "size": 2154},
+                {"name": "failures", "size": 0}
+            ]
+        },
+        {
+            "name": "200+",
+            "children": [
+                {"name": "successes", "size": 64},
+                {"name": "failures", "size": 0}
+            ]
+        }
+    ]
+};
+
+// Publishing:[35445, [20602, 0], [1900, 0], [539, 0], [504, 401], [10038, 10037], [1862, 1862]]
+var publishing = {
+    "name": "Publishing",
+    "children": [
+        {
+            "name": "0-25",
+            "children": [
+                {"name": "successes", "size": 0},
+                {"name": "failures", "size": 20602}
+            ]
+        },
+        {
+            "name": "26-50",
+            "children": [
+                {"name": "successes", "size": 0},
+                {"name": "failures", "size": 1900}
+            ]
+        },
+        {
+            "name": "51-75",
+            "children": [
+                {"name": "successes", "size": 0},
+                {"name": "failures", "size": 539}
+            ]
+        },
+        {
+            "name": "76-100",
+            "children": [
+                {"name": "successes", "size": 401},
+                {"name": "failures", "size": 103}
+            ]
+        },
+        {
+            "name": "101-200",
+            "children": [
+                {"name": "successes", "size": 10037},
+                {"name": "failures", "size": 1}
+            ]
+        },
+        {
+            "name": "200+",
+            "children": [
+                {"name": "successes", "size": 1862},
+                {"name": "failures", "size": 0}
+            ]
+        }
+    ]
+};
+// 0-25 #5254a3
+// 26-50 #6b6ecf
+// 51-75 #9c9ede
+// 76-100 #637939
+// 101-200 #b5cf6b
+// 200 + #8ca252
+// Games:[28521, [13213, 0], [1829, 0], [732, 0], [317, 88], [6368, 6368], [6062, 6062]]
+var games = {
+    "name": "Games",
+    "children": [
+        {
+            "name": "0-25",
+            "children": [
+                {"name": "successes", "size": 0},
+                {"name": "failures", "size": 13213}
+            ]
+        },
+        {
+            "name": "26-50",
+            "children": [
+                {"name": "successes", "size": 0},
+                {"name": "failures", "size": 1829}
+            ]
+        },
+        {
+            "name": "51-75",
+            "children": [
+                {"name": "successes", "size": 0},
+                {"name": "failures", "size": 732}
+            ]
+        },
+        {
+            "name": "76-100",
+            "children": [
+                {"name": "successes", "size": 88},
+                {"name": "failures", "size": 229}
+            ]
+        },
+        {
+            "name": "101-200",
+            "children": [
+                {"name": "successes", "size": 6383},
+                {"name": "failures", "size": 0}
+            ]
+        },
+        {
+            "name": "200+",
+            "children": [
+                {"name": "successes", "size": 6062},
+                {"name": "failures", "size": 0}
+            ]
+        }
+    ]
+};
